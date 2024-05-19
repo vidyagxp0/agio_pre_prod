04eab715620af4f2256927d05b31e2edee747e60
akashmis2525 <mishraakash576@gmail.com>
2024-05-19 12:07:59 -0700

    lab_incident_table

--- database/migrations/2023_12_12_105938_create_lab_incidents_table.php
+++ database/migrations/2023_12_12_105938_create_lab_incidents_table.php
@@ -84,6 +84,60 @@ return new class extends Migration
             $table->string('review_completed_on')->nullable();
             $table->string('all_activities_completed_by')->nullable();
             $table->string('all_activities_completed_on')->nullable();
+          
+            // new added -- General Information
+
+            $table->longtext('incident_involved_others_gi')->nullable();
+            $table->longtext('description_incidence_gi')->nullable();
+            $table->longtext('stage_stage_gi')->nullable();
+            $table->longtext('incident_stability_cond_gi')->nullable();
+            $table->string('incident_interval_others_gi')->nullable();
+            $table->string('test_gi')->nullable();
+            $table->string('date_gi')->nullable();
+            $table->string('incident_date_analysis_gi')->nullable();
+            $table->string('incident_specification_no_gi')->nullable();
+            $table->string('incident_stp_no_gi')->nullable();
+            $table->string('Incident_name_analyst_no_gi')->nullable();
+            $table->string('incident_date_incidence_gi')->nullable();
+            $table->string('analyst_sign_date_gi')->nullable();
+            $table->string('section_sign_date_gi')->nullable();
+            $table->longtext('attachments_gi')->nullable();
+            // $table->longtext('section_sign_date_gi')->nullable();
+
+            //new added --Immediate Actions
+
+            $table->longtext('immediate_action_ia')->nullable();
+            $table->string('immediate_date_ia')->nullable();
+            $table->string('section_date_ia')->nullable();
+            $table->longtext('details_investigation_ia')->nullable();
+            $table->longtext('proposed_correctivei_ia')->nullable();
+            $table->longtext('repeat_analysis_plan_ia')->nullable();
+            $table->longtext('result_of_repeat_analysis_ia')->nullable();
+            $table->longtext('corrective_and_preventive_action_ia')->nullable();
+            $table->string('capa_number_im')->nullable();
+            $table->longtext('investigation_summary_ia')->nullable();
+            $table->string('type_incidence_ia')->nullable();
+            $table->longtext('attachments_ia')->nullable();
+            // Extension
+            $table->longtext('reasoon_for_extension_e')->nullable();
+            $table->longtext('extension_date_e')->nullable();
+            $table->string('extension_date_fe')->nullable();
+            $table->string('extension_date_initiator')->nullable();
+            $table->longtext('reasoon_for_extension_esc')->nullable();
+            $table->string('extension_date_esc')->nullable();
+            $table->string('extension_date_idsc')->nullable();
+            $table->longtext('reasoon_for_extension_tc')->nullable();
+            $table->string('extension_date__tc')->nullable();
+            $table->string('extension_date_idtc')->nullable();
+            $table->longtext('extension_attachments_e')->nullable();
+            
+
+
+            
+
+
+
+
             // $table->string('cancelled_by')->nullable();
             $table->string('cancelled_on')->nullable();
             $table->timestamps();
