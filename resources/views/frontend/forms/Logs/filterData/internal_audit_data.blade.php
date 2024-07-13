5b6afea29ea5d8319c50d5a0dbf8c8326786d6b0
akashmishra <mishraakash576@gmail.com>
2024-07-13 17:22:17 +0530

    internal audit date format changes done

--- resources/views/frontend/forms/Logs/filterData/internal_audit_data.blade.php
+++ resources/views/frontend/forms/Logs/filterData/internal_audit_data.blade.php
@@ -1,8 +1,15 @@
+@php
+    use Carbon\Carbon;
+@endphp
 @forelse($internal_audi as $logs)
 <tr>
     
 <td>{{$loop->index+1}}</td>
-<td>{{$logs->intiation_date}}</td>
+@if($logs->intiation_date)
+    <td>{{ Carbon::createFromFormat('Y-m-d', $logs->intiation_date)->format('d-M-Y') }}</td> 
+    @else
+        NA
+    @endif
 <td>{{$logs->division? $logs->division->name: '-' }}/IA/{{ Helpers::year($logs->created_at) }}/{{ $logs->record }}</td>
 <td>{{$logs->initiator? $logs->initiator->name : '-'}}</td>
 <td>{{$logs->short_description}}</td>
@@ -11,8 +18,11 @@
 <td>{{$logs->leadAuditor ? $logs->leadAuditor->name : 'NA'}}</td>
 <td>{{$logs->initiator_group_code}}</td>
 <td>{{$logs->division ? $logs->division->name : '-'}}</td>
-<td>{{$logs->due_date}}</td>
-<td>{{$logs->audit_lead_more_info_reqd_on ? $logs->audit_lead_more_info_reqd_on:'NA'}}</td>
+@if($logs->due_date)
+    <td>{{ Carbon::createFromFormat('Y-m-d', $logs->due_date)->format('d-M-Y') }}</td> 
+    @else
+        NA
+    @endif<td>{{$logs->audit_lead_more_info_reqd_on ? $logs->audit_lead_more_info_reqd_on:'NA'}}</td>
 <td>{{$logs->status}}</td>        
 <tr>
 
