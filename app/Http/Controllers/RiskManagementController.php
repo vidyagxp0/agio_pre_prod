4afa53d00d3cdf2fc8ee99247b2b4a3dc787342f
vaibhav-awar <vaibhav.vidyagxp@gmail.com>
2024-08-09 10:58:00 +0530

    removeextramodel

--- app/Http/Controllers/RiskManagementController.php
+++ app/Http/Controllers/RiskManagementController.php
@@ -10,7 +10,6 @@ use App\Models\RoleGroup;
 use App\Models\User;
 use App\Models\RiskManagmentCft;
 use App\Models\RiskAssesmentCftResponce;
-use App\Models\RiskManagmentCft;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
