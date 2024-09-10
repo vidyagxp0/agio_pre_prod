73bbb64ff36f160df9b333e0fbd1ef5278f88a9b
akashmishra <mishraakash576@gmail.com>
2024-09-10 10:15:00 +0530

    ooc andd logs changes done

---
+++ database/migrations/2024_09_10_094304_add_column_to_out_of_calibrations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('out_of_calibrations', function (Blueprint $table) {
+            $table->text('initiated_through_capas_ooc_IB')->nullable();
+            $table->text('initiated_through_capa_prevent_ooc_IB')->nullable();
+            $table->text('initiated_through_capa_corrective_ooc_IB')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('out_of_calibrations', function (Blueprint $table) {
+            //
+        });
+    }
+};
