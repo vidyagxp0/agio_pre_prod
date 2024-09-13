5c629d5fc308e8ae97958688fa05f11756fd1515
nilesh-2000 <163507962+nilesh-2000@users.noreply.github.com>
2024-09-13 12:05:21 +0530

    update

---
+++ app/Models/TNI.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TNI extends Model
+{
+    use HasFactory;
+}
