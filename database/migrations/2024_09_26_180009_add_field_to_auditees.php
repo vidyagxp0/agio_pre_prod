863aca86ec4cec03b6f302d7d283f96f97dd78a9
Ashish <156294206+AshishVerma777@users.noreply.github.com>
2024-09-26 18:07:25 +0530

    done

---
+++ database/migrations/2024_09_26_180009_add_field_to_auditees.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('auditees', function (Blueprint $table) {
+            $table->text('reviewer_person_value')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('auditees', function (Blueprint $table) {
+            //
+        });
+    }
+};
