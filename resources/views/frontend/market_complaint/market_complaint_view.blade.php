28a7991e8b68a8c02aba818e9ee4655923d5d6a9
Ashish <156294206+AshishVerma777@users.noreply.github.com>
2024-10-08 09:59:45 +0530

    done

--- resources/views/frontend/market_complaint/market_complaint_view.blade.php
+++ resources/views/frontend/market_complaint/market_complaint_view.blade.php
@@ -2124,7 +2124,6 @@
                                                                     </div>
                                                                 </div>
                                                             </td>
-                                                            
                                                             <td>
                                                                 <div class="new-date-data-field">
                                                                     <div class="group-input input-date">
