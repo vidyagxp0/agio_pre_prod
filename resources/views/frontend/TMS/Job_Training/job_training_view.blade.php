fab86af719e0efc69b30daf7c9e2656a1a243e4f
Harsh <107297786+harshmerchant@users.noreply.github.com>
2024-10-22 18:59:47 +0530

    update question nd certificate

--- resources/views/frontend/TMS/Job_Training/job_training_view.blade.php
+++ resources/views/frontend/TMS/Job_Training/job_training_view.blade.php
@@ -1067,8 +1067,8 @@
                             <div class="row">
                                 <div class="col-lg-12">
                                 <div class="button-block">
-                                        <button type="button" class="printButton" onclick="printCertificate()">
-                                            <i class="fas fa-print"></i>Print
+                                        <button type="button" class="printButton" onclick="downloadCertificate()">
+                                            <i class="fas fa-print"></i> Print
                                         </button>
                                 </div>
 
@@ -1139,6 +1139,7 @@
                 <!-- CSS Styling -->
             <style>
               @import url('https://fonts.googleapis.com/css?family=Open+Sans|Pinyon+Script|Rochester');
+ 
 
                 .cursive {
                     font-family: 'Pinyon Script', cursive;
@@ -1167,7 +1168,6 @@
                 }
 
                 .pm-empty-space {
-                    height: 40px; 
                     width: 100%;
                 }
 
@@ -1181,6 +1181,7 @@
                     font-family: 'Open Sans', sans-serif;
                     box-shadow: 0 9px 15px rgb(18 5 23 / 60%);
                     margin-top: 35px;
+                    align-items: center !important;  
                    
                 }
  
@@ -1333,7 +1334,7 @@
                         box-shadow: none; 
                         background-color: #618597; 
                         // padding: 30px;
-                        margin: 0 auto; 
+                        // margin: 0 auto; 
                     }
 
                     .outer-border, .inner-border {
@@ -1363,11 +1364,10 @@
                     function downloadCertificate() {
                         const element = document.querySelector('.pm-certificate-container');
                         const options = {
-                            margin: 0, // Margin ko adjust karen, yahan 0 set kiya hai
+                            margin: 0, 
                             filename: 'Job-Training-certificate.pdf',
                             html2canvas: { 
                                 scale: 2,
-                                // Allowing the certificate to be centered on the canvas
                                 x: 0,
                                 y: 0
                             },
