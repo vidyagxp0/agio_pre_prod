ab32c750da393cf9fb5f0edf0ccc40b9fa7310c0
kp <kuldeepoffice17@gmail.com>
2025-04-15 16:36:53 +0530

    correction spelling

--- resources/views/frontend/incident/single-report.blade.php
+++ resources/views/frontend/incident/single-report.blade.php
@@ -669,7 +669,7 @@
                 </div>
                 <table>
                     <tr>
-                        <th class="w-20">Review Of Incident And Verification Of Effectiveness Of Corrcetion</th>
+                        <th class="w-20">Review Of Incident And Verification Of Effectiveness Of Correction</th>
                         <td class="w-80">
                             @if ($data->review_of_verific)
                                 {{ $data->review_of_verific }}
