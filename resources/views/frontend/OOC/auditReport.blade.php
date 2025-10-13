48271e8e3d619365636cdefcbf714d2337da4c89
Harsh chhari <chhariharsh257@gmail.com>
2025-10-13 13:23:25 +0530

    Revert "commit"
    This reverts commit a53c3de2464a17da91c0aaec54b60412bc3141f4.

--- resources/views/frontend/OOC/auditReport.blade.php
+++ resources/views/frontend/OOC/auditReport.blade.php
@@ -10,115 +10,151 @@
 </head>
 
 <style>
-    @page {
-         margin: 160px 35px 100px; /* top header, side margin, bottom footer */
-     }
     body {
         font-family: 'Roboto', sans-serif;
         margin: 0;
         padding: 0;
-        font-size: 11px;
-        line-height: 1.4;
-        color: #000;
-        margin-top: 20px;
-         margin-bottom: -40px; 
+        min-width: 100vw;
+        min-height: 100vh;
     }
 
-    header, footer {
-        position: fixed;
-        left: 0;
-        right: 0;
-        padding: 20px 35px;
-        font-size: 12px;
-        box-sizing: border-box;
+    .w-5 {
+        width: 5%;
     }
 
-    header {
-        top: -140px;
-        border-bottom: none;
+    .w-10 {
+        width: 10%;
+    }
+    .w-15 {
+        width: 15%;
     }
 
-    footer {
-        bottom: 0;
-        bottom: -120px;
-        border-top: none;
+    .w-20 {
+        width: 20%;
     }
 
-    .logo img {
-        display: block;
-        margin-left: auto;
-    }
-    /* To remove borders from content part only */
-    .content-area table {
-        border: none !important;
+    .w-30 {
+        width: 30%;
     }
 
-    .inner-block {
-        padding: 20px 35px; /* Add margin from left/right and top/bottom */
-        box-sizing: border-box;
-    }
-    
-    .block {
-        margin-bottom: 25px;
+    .w-40 {
+        width: 40%;
     }
 
-    .block-head {
-        font-size: 13px;
-        font-weight: bold;
-        border-bottom: 2px solid #387478;
-        color: #387478;
-        margin-bottom: 10px;
-        padding-bottom: 5px;
+    .w-50 {
+        width: 50%;
     }
 
-    .table_bg {
-        background-color: #387478;
-        color: #111;
+    .w-60 {
+        width: 60%;
+    }
+
+    .w-70 {
+        width: 70%;
+    }
+
+    .w-80 {
+        width: 80%;
+    }
+
+    .w-90 {
+        width: 90%;
+    }
+
+    .w-100 {
+        width: 100%;
+    }
+
+    .h-100 {
+        height: 100%;
+    }
+
+    table,
+    th,
+    td {
+        border: 1px solid black;
+        border-collapse: collapse;
+        font-size: 0.9rem;
+        overflow: hidden;
+        vertical-align: middle;
     }
 
     table {
         width: 100%;
-        border-collapse: collapse;
-        margin-bottom: 12px;
     }
 
-    th, td {
-        padding: 6px 10px;
-        font-size: 10.5px;
-        border: 1px solid #ccc;
+    th,
+    td {
+        padding: 10px;
         text-align: left;
-        vertical-align: top;
     }
 
-    th {
-        background-color: #f2f2f2;
-        font-weight: 600;
+    header .head {
+        font-weight: bold;
+        text-align: center;
+        font-size: 1.2rem;
     }
 
-    .section-gap {
-        margin-top: 20px;
+    @page {
+        size: A4;
+        margin-top: 160px;
+        margin-bottom: 60px;
     }
 
-    .no-border th, .no-border td {
-        border: none !important;
+    header {
+        position: fixed;
+        top: -140px;
+        left: 0;
+        width: 100%;
+        display: block;
     }
 
-    /* .w-5 { width: 5%; } */
-    .w-5 { width: 6%; }
-    .w-8 { width: 8%; }
-    .w-10 { width: 10%; }
-    .w-20 { width: 20%; }
-    .w-30 { width: 30%; }
-    .w-50 { width: 50%; }
-    .w-70 { width: 70%; }
-    .w-80 { width: 80%; }
-    .w-100 { width: 100%; }
-    .text-center { text-align: center; }
-    .border-table {
-        overflow-x: auto;
+    footer {
+        position: fixed;
+        bottom: -40px;
+        left: 0;
+        width: 100%;
     }
 
-    table th, table td {
+    .inner-block {
+        padding: 10px;
+    }
+
+    .inner-block .head {
+        font-weight: bold;
+        font-size: 1.2rem;
+        margin-bottom: 5px;
+    }
+
+    .inner-block .division {
+        margin-bottom: 10px;
+    }
+
+    .first-table {
+        border-top: 1px solid black;
+        margin-bottom: 20px;
+    }
+
+    .first-table table td,
+    .first-table table th,
+    .first-table table {
+        border: 0;
+    }
+
+    .second-table td:nth-child(1)>div {
+        margin-bottom: 10px;
+    }
+
+    .second-table td:nth-child(1)>div:nth-last-child(1) {
+        margin-bottom: 0px;
+    }
+
+    .table_bg {
+        background: #4274da57;
+    }
+
+    .allow-wb {
+        word-break: break-all;
         word-wrap: break-word;
     }
 </style>
@@ -129,7 +165,7 @@
         <table>
             <tr>
                 <td class="w-70 head">
-                    <strong>Out Of Calibration Audit Trail Report</strong>
+                    Out Of Calibration Audit Trail Report
                 </td>
                 <td class="w-30">
                     <div class="logo" style="text-align: center;">
@@ -172,9 +208,10 @@
 
     <div class="inner-block">
 
+
         <div class="second-table">
-            <table>
-                <thead>
+            <table class="allow-wb" style="table-layout: fixed; width: 700px;">>
+                <thead style="max-width: 100px;">
                     <tr class="table_bg">
                         <th class="w-5">S.No</th>
                         <th class="w-15">Flow Changed From</th>
