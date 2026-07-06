5aae7d5c473afc22d85e2c0f0dff99320d8f16aa
SUNILPATELGXP <sunil.patel.gxp@gmail.com>
2026-07-06 17:13:02 +0530

    update code

--- app/Http/Middleware/PreventBackHistory.php
+++ app/Http/Middleware/PreventBackHistory.php
@@ -15,14 +15,14 @@ class PreventBackHistory
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-public function handle(Request $request, Closure $next)
-{
-    $response = $next($request);
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
 
-    $response->headers->set('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
-    $response->headers->set('Pragma', 'no-cache');
-    $response->headers->set('Expires', 'Sat, 01 Jan 2000 00:00:00 GMT');
+        $response->headers->set('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
+        $response->headers->set('Pragma', 'no-cache');
+        $response->headers->set('Expires', 'Sat, 01 Jan 2000 00:00:00 GMT');
 
-    return $response;
-}
+        return $response;
+    }
 }
